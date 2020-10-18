2a032971492c518308b14fa7c847a26186902ec8
Thomaz Nathanael <51930484+thomaznathanael@users.noreply.github.com>
2020-10-18 08:14:50 -0300

    Create __init__.py

---
+++ pycep/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import json
+import requests
+from sys import exit
+
+__version__ = '1.0.0'
+
+class PyCep:
+    def __init__(self, cep):
+        response = requests.get('http://www.viacep.com.br/ws/%s/json' % cep)
+        if response.status_code == 200:
+            self.__dadosCep = response.json()
+
+    @property
+    def dadosCep(self):
+        return self.__dadosCep
