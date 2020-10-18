5c4ea775a052799123359b093b65196d9173fb5c
Thomaz Nathanael <51930484+thomaznathanael@users.noreply.github.com>
2020-10-18 08:36:28 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,43 @@
+
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+from setuptools import find_packages
+
+import pycep
+
+version = pycep.__version__
+
+install_requires = [
+    'requests>=2.9.1'
+]
+
+setup(
+    name='PyCep',
+    version=version,
+    author='Thomaz Nathanael',
+    author_email='thomaznathanael@gmail.com',
+    scripts=['exemplo.py', 'README.md'],
+    url='https://github.com/thomaznathanael/PyCep',
+    license='LICENSE',
+    description="Um simples modulo para obter informacoes de um CEP",
+    long_description="Com este modulo é possível obter várias informações dos CEPs do Brasil, como logradouro, cidade, estado etc...",
+    install_requires=install_requires,
+    platforms = 'any',
+    packages=find_packages(),
+    classifiers = [
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Development Status :: 4 - Beta',
+        'Natural Language :: English',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],	
+)
+
